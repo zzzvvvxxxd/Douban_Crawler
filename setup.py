4625dbb1edf589e37d00888d22f5359cd207d549
zzzvvvxxxd <zzzvvvxxxd@163.com>
2015-09-14 15:24:42 +0800

    book模块修改

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Douban',
+    version='0.0.1',
+    packages=['douban_util'],
+    url='',
+    license='MIT',
+    author='LoveLYJ',
+    author_email='zzzvvvxxxd@gmail.com',
+    description=u'抓取豆瓣内容模组'
+)
