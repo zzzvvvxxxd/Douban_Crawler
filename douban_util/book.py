886bde1d27f2f9a6d7292c3d9cf7882c358366bb
zzzvvvxxxd <zzzvvvxxxd@163.com>
2015-09-23 13:06:39 +0800

    null

--- douban_util/book.py
+++ douban_util/book.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python2.7
 # coding=utf-8
 __author__ = 'bulu_dog'
-import time
 import requests
 import multiprocessing
 from bs4 import BeautifulSoup
@@ -18,27 +17,27 @@ def search_book(query, page):
     :return: book url list
     """
     params = {
-        'cat':1001,
-        'search_text':query,
-        'start':page * 15
+        'cat': 1001,
+        'search_text': query,
+        'start': page * 15
     }
     r = requests.get(URL, params=params)
     soup = BeautifulSoup(r.content, 'lxml')
     # 获取图书列表
-    ul_soup = soup.findAll('div', attrs = {'class': 'info'})
+    ul_soup = soup.findAll('div', attrs={'class': 'info'})
     book_list = []
     for item in ul_soup:
         a = item.a
         book_list.append(
             [
-                a.get_text().replace('\n', '').replace(' ' ,''),     # 标题
+                a.get_text().replace('\n', '').replace(' ', ''),     # 标题
                 a['href']                                           # URL
             ]
         )
     return book_list
 
 
-def get_info(query):
+def get_info_to_md(query):
     """
     get all books' info in first two search pages
     :param query:
@@ -56,13 +55,13 @@ def get_info(query):
         books = search_book(query, i)
         for book in books:
             name = book[0]
-            url  = book[1]
+            url = book[1]
             result.append(pool.apply_async(get_single_book_info, (url, )))
     pool.close()
     pool.join()
     for item in result:
-        item  = item.get()
-        name  = item['name'].encode('utf-8')
+        item = item.get()
+        name = item['name'].encode('utf-8')
         image = item['img']
         out_file.write("###{name}  \n".format(name=name))
         out_file.write("![{name}]({url})  \n".format(name=name, url=image))
@@ -77,19 +76,19 @@ def get_single_book_info(url):
     """
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
-    name = soup.h1.get_text().replace(' ','').replace('\n','')
-    info = soup.find('div', attrs={'id':'info'})
-    #print info.get_text().replace(' ','').splitlines()
+    name = soup.h1.get_text().replace(' ', '').replace('\n', '')
+    info = soup.find('div', attrs={'id': 'info'})
     result = []
     for item in info.get_text().splitlines():
         item = item.strip()
         if len(item) > 1:
             result.append(item)
-    score = soup.find('strong', attrs={'class':'rating_num'})
-    image = soup.find('div', attrs={'id':'mainpic'})
-    return dict(name=name, score=score.get_text().strip(),info=result,img=image.img['src'])
-
+    score = soup.find('strong', attrs={'class': 'rating_num'})
+    image = soup.find('div', attrs={'id': 'mainpic'})
+    return dict(name=name,
+                score=score.get_text().strip(),
+                info=result,
+                img=image.img['src'])
 
 if __name__ == '__main__':
-    get_info('数据挖掘')
-    #print get_single_book_info('http://book.douban.com/subject/1102235/')
+    get_info_to_md('数据挖掘')
---
+++ douban_util/movie.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+__author__ = 'bulu_dog'
+import requests
+import multiprocessing
+from bs4 import BeautifulSoup
+
+URL = ''
+
+
+def search_movie():
+    """
+    在豆瓣电影中查找电影，并返回第一条结果的信息
+    :param query
+    :param page
+    """
+    pass
